490a1c8700edfc2b100ea4b12a538dbef5ec2139
ajiboyesunkanmisunday-rgb <ajiboyesunkanmisunday@gmail.com>
2026-04-19 00:00:40 +0100

    feat(celebrations): add celebration CRUD pages and wire thanksgiving actions (#10)
    - Add Celebration type covering birthdays, anniversaries, thanksgiving, child dedication
    - Add celebrations mock data (15 entries)
    - Add Add Celebration button on page header
    - Wire thanksgiving View/Edit to route navigation
    - New celebration create, detail, edit pages

---
+++ frontend/src/app/celebrations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import PageClient from "./PageClient";
+
+export function generateStaticParams() {
+  return Array.from({ length: 15 }, (_, i) => ({
+    id: `cel-${i + 1}`,
+  }));
+}
+
+export default function Page() {
+  return <PageClient />;
+}
